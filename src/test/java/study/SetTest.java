2c405c2e2b1c2f65787668c9aae05e7070596a26
DAOU-8095\daou <kihyunkim@daou.co.kr>
2021-08-19 09:41:42 +0900

    0. 학습테스트 실습

---
+++ src/test/java/study/SetTest.java
@@ -0,0 +1,48 @@
+package study;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class SetTest {
+    private Set<Integer> numbers;
+
+    @BeforeEach
+    void setUp() {
+        numbers = new HashSet<>();
+        numbers.add(1);
+        numbers.add(1);
+        numbers.add(2);
+        numbers.add(3);
+    }
+
+    @DisplayName("Set의 size() 메소드를 활용해 Set의 크기를 확인하는 학습테스트를 구현한다.")
+    @Test
+    void quest1() {
+        assertThat(numbers.size()).isEqualTo(3);
+    }
+
+    @DisplayName("Set의 contains() 메소드를 활용해 1, 2, 3의 값이 존재하는지를 확인하는 학습테스트를 구현하려한다. with Parameterized")
+    @ParameterizedTest
+    @ValueSource(ints = {1, 2, 3})
+    void quest2(int i) {
+        assertThat(numbers.contains(i)).isTrue();
+    }
+
+    @DisplayName("Set의 contains() 메소드를 활용해 1, 2, 3의 값이 존재하는지를 확인하는 학습테스트를 구현하려한다. with Parameterized")
+    @ParameterizedTest
+    @CsvSource(value = {"1:true", "2:true", "3:true", "4:false", "5:false"}, delimiter = ':')
+    void quest3(Integer input, Boolean expected) {
+        assertThat(numbers.contains(input)).isEqualTo(expected);
+    }
+}
--- src/test/java/study/StringTest.java
+++ src/test/java/study/StringTest.java
@@ -1,8 +1,10 @@
 package study;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 public class StringTest {
     @Test
@@ -10,4 +12,40 @@ public class StringTest {
         String actual = "abc".replace("b", "d");
         assertThat(actual).isEqualTo("adc");
     }
+
+    @DisplayName("1,2 을 ,로 split 했을 때 1과 2로 잘 분리되는지 확인하는 학습 테스트를 구현한다.")
+    @Test
+    void quest1() {
+        String[] split = "1,2".split(",");
+        assertThat(split).containsExactly("1", "2");
+    }
+    @DisplayName("1\"을 ,로 split 했을 때 1만을 포함하는 배열이 반환되는지에 대한 학습 테스트를 구현한다.")
+    @Test
+    void quest1_1() {
+        String[] split = "1".split(",");
+        assertThat(split).containsExactly("1");
+    }
+
+    @DisplayName("(1,2)\" 값이 주어졌을 때 String의 substring() 메소드를 활용해 ()을 제거하고 \"1,2\"를 반환하도록 구현한다.")
+    @Test
+    void quest2() {
+        String substring = "(1,2)".substring(1, 4);
+        assertThat(substring).isEqualTo("1,2");
+    }
+
+    @DisplayName("abc 값이 주어졌을 때 String의 charAt() 메소드를 활용해 특정 위치의 문자를 가져오는 학습 테스트를 구현한다")
+    @Test
+    void quest3() {
+        char c = "abc".charAt(2);
+        assertThat(c).isEqualTo('c');
+    }
+
+    @DisplayName("abc 값이 주어졌을 때 String의 charAt() 메소드를 활용해 특정 위치의 문자를 가져오는 학습 테스트를 구현한다")
+    @Test
+    void quest3_1() {
+        assertThatThrownBy(() -> "abc".charAt(4))
+                .isInstanceOf(StringIndexOutOfBoundsException.class)
+                .hasMessageContaining("String index out of range: 4");
+    }
+
 }
